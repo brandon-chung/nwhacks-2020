cde08c7837fed106cbb2b586022fdfb8884ec4ac
Brandon Chung <b.chung8612@gmail.com>
2020-01-11 17:14:39 -0800

    Finished implementing topic corpuses

---
+++ server/topics.py
@@ -0,0 +1,17 @@
+"""
+Module containing topic corpuses.
+
+"""
+
+
+fitness = ['gym', 'workout', 'work out', 'squat', 'deadlift',
+            'fit', 'fitness', 'exercise', 'reps', 'pr', 'run', 'swim', 'jog']
+
+academic = ['studied', 'study', 'studying', 'grades', 'assignment',
+            'finished assignment', 'exam', 'test', 'midterm', 'finals']
+
+career = ['job', 'overtime', 'finished project', 'completed project', 
+            'salary', 'earned', 'shift', 'promotion', 'promoted', 'new job', 'job offer', 'interview']
+
+social = ['party', 'hangout', 'hang out', 'date', 'rave', 'get together', 'friends', 'buddies', 'bros', 'friend',
+'girlfriend', 'boyfriend', 'hanging out', 'vibing', 'happy hour']
